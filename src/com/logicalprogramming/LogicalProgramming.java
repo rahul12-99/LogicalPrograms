60842e74e2bf17fd44614368fa46ecd97a7bae00
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-03-07 15:52:31 +0530

    Added: Code for generate a random coupon number

--- src/com/logicalprogramming/LogicalProgramming.java
+++ src/com/logicalprogramming/LogicalProgramming.java
@@ -3,21 +3,33 @@ package com.logicalprogramming;
 import java.util.Scanner;
 
 public class LogicalProgramming {
+
+    public  static int randomGenerator(int size){
+        return (int)(Math.floor(Math.random() * size)+ 1);
+    }
     public static void main(String[] args) {
-
-        int num, reminder = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number");
-        num = sc.nextInt();
-
-        while (num > 0){
-
-            reminder = num % 10;
-            System.out.print(reminder + " ");
-            num = num/10;
-
+        int number;
+        int random;
+        System.out.println("Kindly enter the total number of coupons to generate");
+        number = sc.nextInt();
+        int[] coupons = new int[number];
+        for (int i = 0; i < number; i++) {
+            random = randomGenerator(number);           //store random number by calling
+            coupons[i] = random;                        //add random no in array
+            for (int j = 0; j < i; j++) {
+                if (coupons[i] == coupons[j]) {
+                    i--;                               //Be on same index until unique number
+                }
+            }
         }
-
+        for (int i:coupons) {
+            System.out.println(i);
+        }
     }
 }
+
+
+
+
 
