3956f5bcefbf14eae5c5799f43c1191a91549f69
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-03-07 12:04:12 +0530

    Added: Welcome message

---
+++ src/com/logicalprogramming/LogicalProgramming.java
@@ -0,0 +1,7 @@
+package com.logicalprogramming;
+
+public class LogicalProgramming {
+    public static void main(String[] args) {
+        System.out.println("Welcome to logical programs");
+    }
+}
