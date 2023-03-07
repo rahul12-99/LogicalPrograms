103965d4f13fc2d7757f28e6a9837bbccbec051a
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-03-07 15:14:43 +0530

    Added: Code for getting fibonacci series

--- src/com/logicalprogramming/LogicalProgramming.java
+++ src/com/logicalprogramming/LogicalProgramming.java
@@ -1,7 +1,22 @@
 package com.logicalprogramming;
 
+import java.util.Scanner;
+
 public class LogicalProgramming {
     public static void main(String[] args) {
-        System.out.println("Welcome to logical programs");
+
+        int term, a =0, b =1 , c;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter term");
+        term = sc.nextInt();
+
+        for(int i = 1; i <= term; i++){
+            System.out.print(a + " ");
+            c = a +  b;
+            a = b;
+            b = c;
+        }
+
     }
 }
+
