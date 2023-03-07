f7d5529e015b95e55cdc500d36c78c1f1b5e97d9
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-03-07 15:38:15 +0530

    Added: Code for reverse a number

--- src/com/logicalprogramming/LogicalProgramming.java
+++ src/com/logicalprogramming/LogicalProgramming.java
@@ -5,22 +5,19 @@ import java.util.Scanner;
 public class LogicalProgramming {
     public static void main(String[] args) {
 
-        int num, count = 0;
+        int num, reminder = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number");
         num = sc.nextInt();
 
-        for (int i = 1; i <= num; i++) {
-            if (num % i == 0) {
-                count++;
-            }
-        }
-        if (count == 2) {
-            System.out.println("prime number");
-        } else {
-            System.out.println("not a prime number");
-        }
-    }
+        while (num > 0){
 
+            reminder = num % 10;
+            System.out.print(reminder + " ");
+            num = num/10;
+
+        }
+
+    }
 }
 
